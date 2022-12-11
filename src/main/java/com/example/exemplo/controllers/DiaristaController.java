40276872b93615be05263e30ffef7dbad2865cd2
gabrielb3r <gabrielb3rnardo@gmail.com>
2022-12-11 20:04:24 -0300

    detalhes diarista

--- src/main/java/com/example/exemplo/controllers/DiaristaController.java
+++ src/main/java/com/example/exemplo/controllers/DiaristaController.java
@@ -2,6 +2,8 @@ package com.example.exemplo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,12 +15,20 @@ public class DiaristaController {
 
     @Autowired
     private DiaristaRepository repository;
-    
-    public ModelAndView buscarTodos(){
+
+    @GetMapping
+    public ModelAndView buscarTodos() {
 
         var modelAndView = new ModelAndView("listar_diaristas");
         modelAndView.addObject("diaristas", repository.findAll());
         return modelAndView;
 
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView buscarPorId(@PathVariable Long id) {
+        var modelAndView = new ModelAndView("detalhes");
+        modelAndView.addObject("diarista", repository.findById(id));
+        return modelAndView;
     }
 }
